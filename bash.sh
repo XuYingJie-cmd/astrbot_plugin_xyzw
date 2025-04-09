e0fcea4208daa5992e233b271502af1f40fcaac9
徐英杰 <806895158@qq.com>
2025-04-09 19:44:32 +0800

    修改文档描述

--- bash.sh
+++ bash.sh
@@ -11,17 +11,17 @@ log() {
 }
 
 
-# 安装 Python 包
-log "正在执行: 安装 Python 包"
-pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
-if [ $? -ne 0 ]; then
-    log "安装 Python 包失败"
-    exit 1
+
+# 检查并创建 sources.list 文件
+if [ ! -f /etc/apt/sources.list ]; then
+    log "创建 /etc/apt/sources.list 文件"
+    # 这里假设使用 Ubuntu 20.04 (focal) 的源，根据实际情况修改
+    echo "deb http://archive.ubuntu.com/ubuntu focal main restricted universe multiverse" | sudo tee /etc/apt/sources.list
 fi
 
 # 更新依赖包列表
 log "正在执行: 更新依赖包列表"
-apt-get update -o Dir::Etc::sourcelist="sources.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+sudo apt-get update
 if [ $? -ne 0 ]; then
     log "更新依赖包列表失败"
     exit 1
@@ -29,15 +29,23 @@ fi
 
 # 安装依赖
 log "正在执行: 安装依赖"
-apt-get install -y libgl1-mesa-glx
+sudo apt-get install -y libgl1-mesa-glx
 if [ $? -ne 0 ]; then
     log "安装 libgl1-mesa-glx 失败"
     exit 1
 fi
 
-apt-get install -y libglib2.0-0
+sudo apt-get install -y libglib2.0-0
 if [ $? -ne 0 ]; then
     log "安装 libglib2.0-0 失败"
+    exit 1
+fi
+
+# 安装 Python 包
+log "正在执行: 安装 Python 包"
+pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
+if [ $? -ne 0 ]; then
+    log "安装 Python 包失败"
     exit 1
 fi
 
