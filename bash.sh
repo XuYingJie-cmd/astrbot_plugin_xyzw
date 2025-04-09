3df4bd297dc0e082d480988c36a4fc151844f3f0
徐英杰 <806895158@qq.com>
2025-04-09 21:37:06 +0800

    修改文档描述

--- bash.sh
+++ bash.sh
@@ -5,6 +5,21 @@ log() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
 }
 
+# 备份原有的sources.list文件
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+# 使用阿里云镜像源替换原有内容
+cat << EOF > /etc/apt/sources.list
+deb http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
+deb http://mirrors.aliyun.com/debian-security/ bullseye-security main
+deb-src http://mirrors.aliyun.com/debian-security/ bullseye-security main
+deb http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ bullseye-updates main non-free contrib
+deb http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+deb-src http://mirrors.aliyun.com/debian/ bullseye-backports main non-free contrib
+EOF
+
 # 2. 更新软件包列表
 log "更新软件包列表"
 apt-get update
