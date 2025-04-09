edb153acc0a586ff10ee69fc0d2bddb1bc01553e
徐英杰 <806895158@qq.com>
2025-04-09 11:31:52 +0800

    修改文档描述

--- bash.sh
+++ bash.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# 备份原有的软件源列表
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-
-# 替换为阿里云软件源
-cat << EOF > /etc/apt/sources.list
-deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs) main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs) main restricted universe multiverse
-
-deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs)-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs)-updates main restricted universe multiverse
-
-deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs)-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs)-backports main restricted universe multiverse
-
-deb http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs)-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $(lsb_release -cs)-security main restricted universe multiverse
-EOF
-
 # 更新软件包列表
 apt-get update
 if [ $? -ne 0 ]; then
