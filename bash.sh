39cafdcb22907f902a6a1421ed69780f940892e6
徐英杰 <806895158@qq.com>
2025-04-09 19:46:59 +0800

    修改文档描述

--- bash.sh
+++ bash.sh
@@ -16,12 +16,12 @@ log() {
 if [ ! -f /etc/apt/sources.list ]; then
     log "创建 /etc/apt/sources.list 文件"
     # 这里假设使用 Ubuntu 20.04 (focal) 的源，根据实际情况修改
-    echo "deb http://archive.ubuntu.com/ubuntu focal main restricted universe multiverse" | sudo tee /etc/apt/sources.list
+    echo "deb http://archive.ubuntu.com/ubuntu focal main restricted universe multiverse" |  tee /etc/apt/sources.list
 fi
 
 # 更新依赖包列表
 log "正在执行: 更新依赖包列表"
-sudo apt-get update
+apt-get update
 if [ $? -ne 0 ]; then
     log "更新依赖包列表失败"
     exit 1
@@ -29,13 +29,13 @@ fi
 
 # 安装依赖
 log "正在执行: 安装依赖"
-sudo apt-get install -y libgl1-mesa-glx
+apt-get install -y libgl1-mesa-glx
 if [ $? -ne 0 ]; then
     log "安装 libgl1-mesa-glx 失败"
     exit 1
 fi
 
-sudo apt-get install -y libglib2.0-0
+apt-get install -y libglib2.0-0
 if [ $? -ne 0 ]; then
     log "安装 libglib2.0-0 失败"
     exit 1
