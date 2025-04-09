b7e7f6206f0eb8a1cc7aabfa4f5b35a169c978b8
徐英杰 <806895158@qq.com>
2025-04-09 11:40:33 +0800

    修改文档描述

--- bash.sh
+++ bash.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# 安装 Python 包
+pip install -r requirements.txt -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+if [ $? -ne 0 ]; then
+    echo "安装 Python 包失败"
+    exit 1
+fi
+
+# 备份原有的软件源文件
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+# 替换软件源为阿里云镜像源
+sed -i 's/http:\/\/archive.ubuntu.com\/ubuntu\//http:\/\/mirrors.aliyun.com\/ubuntu\//g' /etc/apt/sources.list
+sed -i 's/http:\/\/security.ubuntu.com\/ubuntu\//http:\/\/mirrors.aliyun.com\/ubuntu\//g' /etc/apt/sources.list
+
 # 更新软件包列表
 apt-get update
 if [ $? -ne 0 ]; then
@@ -14,9 +28,3 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# 安装 Python 包
-pip install -r requirements.txt -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
-if [ $? -ne 0 ]; then
-    echo "安装 Python 包失败"
-    exit 1
-fi
