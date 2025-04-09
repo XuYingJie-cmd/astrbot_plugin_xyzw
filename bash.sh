ea877502293330e27eb3736191a45afeaba314c0
徐英杰 <806895158@qq.com>
2025-04-09 21:33:14 +0800

    修改文档描述

--- bash.sh
+++ bash.sh
@@ -5,12 +5,6 @@ log() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
 }
 
-# 安装 apt-fast
-log "安装 apt-fast"
-sudo add-apt-repository -y ppa:apt-fast/stable
-sudo apt-get update
-sudo apt-get -y install apt-fast
-
 # 1. 配置APT源（更换为清华大学镜像源）
 log "配置APT镜像源"
 tee /etc/apt/sources.list <<EOF
@@ -19,8 +13,18 @@ deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted u
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
 EOF
 
-# 2. 更新软件包列表
+# 更新软件包列表
 log "更新软件包列表"
+apt-get update
+
+# 安装 apt-fast
+log "安装 apt-fast"
+add-apt-repository -y ppa:apt-fast/stable
+apt-get update
+apt-get -y install apt-fast
+
+# 2. 再次更新软件包列表（使用 apt-fast）
+log "再次更新软件包列表（使用 apt-fast）"
 apt-fast update
 
 # 3. 强制更新系统并安装指定包
